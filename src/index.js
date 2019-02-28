18133bea7182c224ba6707302229b9a47faa7804
Elijah McClendon <Elijah@fungry.com>
2019-02-27 17:22:28 -0700

    Creating Math.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import myCurrentLocation, { getGreeting, message, name } from './myModule';
+import add, {subtract} from './math';
 
-console.log(myCurrentLocation);
-console.log(message);
-console.log(name);
-console.log(getGreeting(name));
+// console.log(myCurrentLocation);
+// console.log(message);
+// console.log(name);
+// console.log(getGreeting(name));
+
+console.log(add(2,9));
+console.log(subtract(9,5));
