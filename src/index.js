050b91cdc4e199966883b11222334e35bb6974af
Elijah McClendon <Elijah@fungry.com>
2019-02-28 19:52:48 -0700

    Add Post Query

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {GraphQLServer} from 'graphql-yoga';
 const typeDefs = `
   type Query {
     me: User!
+    post: Post!
   }
   
   type User {
@@ -13,6 +14,13 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+  }
+  
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
@@ -26,6 +34,14 @@ const resolvers = {
         email: 'Michael@example.net'
       };
     },
+    post() {
+      return {
+        id: '123487',
+        title: 'The Thing',
+        body: 'Lorem Ipsum',
+        published: true
+      }
+    }
   }
 };
 
